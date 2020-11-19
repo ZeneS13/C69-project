358b0d44d682eaa4eb94c6a2317728ee9127dcdb
ZeneS13 <sangoi.zene@gmail.com>
2020-11-19 13:03:29 +0530

    C69 project

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ReadStoryScreen from './screens/ReadScreen';
+import WriteStoryScreen from './screens/WriteScreen';
+import ScanScreen from './screens/ScanScreen';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const TabNavigator = createBottomTabNavigator({
+  Read : {screen: ReadStoryScreen},
+  Write : {screen: WriteStoryScreen},
+  Scan: {screen : ScanScreen}
 });
+
+const AppContainer = createAppContainer(TabNavigator);
